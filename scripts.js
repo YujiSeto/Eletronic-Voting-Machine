70eb2176d2594653175936d459d3b3d596cff394
Rodrigo Yuji <rodrigo.y.seto@gmail.com>
2025-08-29 21:35:04 +0900

    feat: Add screen controll

---
+++ scripts.js
@@ -0,0 +1,6 @@
+let yourVoteTo = document.querySelector('.d-1-1 span');
+let position = document.querySelector('.d-1-2 span');
+let description = document.querySelector('.d-1-4');
+let notice = document.querySelector('.d-2');
+let side = document.querySelector('.d-1-right');
+let numbers = document.querySelector('.d-1-3');
---
+++ stages.js
@@ -0,0 +1,46 @@
+let stages = [
+  {
+    title: "COUNCILMAN",
+    numbers: 5,
+    candidates: [
+      {
+        number: "38111",
+        name: "John Doe",
+        party: "ABC",
+        photos: [{ url: "38111.jpg", caption: "Councilman" }],
+      },
+      {
+        number: "77222",
+        name: "Robert Smith",
+        party: "DEFG",
+        photos: [{ url: "77222.jpg", caption: "Councilman" }],
+      },
+    ],
+  },
+  {
+    title: "MAYOR",
+    numbers: 2,
+    candidates: [
+      {
+        number: "99",
+        name: "Michael Johnson",
+        party: "ABC",
+        vice: "Mike",
+        photos: [
+          { url: "99.jpg", caption: "Mayor" },
+          { url: "99_2.jpg", caption: "Vice-Mayor", small: true },
+        ],
+      },
+      {
+        number: "84",
+        name: "David Wilson",
+        party: "QWERTY",
+        vice: "Dave",
+        photos: [
+          { url: "84.jpg", caption: "Mayor" },
+          { url: "84_2.jpg", caption: "Vice-Mayor", small: true },
+        ],
+      },
+    ],
+  },
+];
